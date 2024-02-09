516f06a691de376218f4d028cb7803436d846efa
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 09:42:54 -0300

    ageMonths

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ageMonths;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, por gentileza, digite seu nome: ");
+            string name = Console.ReadLine();
+        Console.WriteLine("Agora, digite sua idade:");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int months = age * 12;
+
+        Console.WriteLine($"{name}, você viveu por {months} meses.");
+
+    }
+}
